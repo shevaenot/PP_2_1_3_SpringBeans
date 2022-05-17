4242f1412c12938adb60ac26c7a7de9b9036a0cd
shevaenot <105449970+shevaenot@users.noreply.github.com>
2022-05-17 20:28:54 +0300

    2.1.3

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,29 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.sql.Time;
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
-    private Animal animal;
+    private Animal animal1;
+    private Animal animal2;
+    private Timer time;
 
+    @Autowired
+    public AnimalsCage(@Qualifier("cat") Animal animal1, @Qualifier("dog") Animal animal2, Timer time){
+        this.animal1 = animal1;
+        this.animal2 = animal2;
+        this.time = time;
+    }
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(animal1.toString() + ", " + animal2.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(time.getTime());
         System.out.println("________________________");
     }
 }
